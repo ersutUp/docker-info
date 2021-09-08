cf05e4d8df09be192c0e60bd34c7e795e092fd2c
ersut <32875090+ersutUp@users.noreply.github.com>
2021-09-08 17:24:41 +0800

    Create java_service.sh

---
+++ jdk8-ffmpeg/systemd/java_service.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+APP_NAME=app
+APP_PATH=/opt/java/
+export JAVA_HOME=/opt/jdk8
+export JRE_HOME=${JAVA_HOME}/jre
+
+usage(){
+  echo "请输入以下内容 [start|stop]"
+  exit 1
+}
+
+stop(){
+	echo "准备关闭当前项目已存在进程"
+	tpid=`ps -ef|grep ${APP_NAME}.jar|grep -v grep|grep -v kill|awk '{print $2}'`
+	if [ ${tpid} ]; then
+	    echo "关闭进程: ${tpid}"
+	    kill -9 $tpid
+	    echo "关闭完成"
+	else
+	    echo '项目未运行'
+	fi
+}
+
+start(){
+	stop
+	echo "开始启动"
+	rm -f ${APP_PATH}${APP_NAME}.pid
+	nohup ${JRE_HOME}/bin/java -jar ${APP_PATH}${APP_NAME}.jar > /dev/null &
+	echo $! > ${APP_PATH}${APP_NAME}.pid
+}
+
+case $1 in
+"start")
+	start
+	;;
+"stop")
+	stop
+	;;
+	*)
+	usage
+	;;
+esac
+exit 0
